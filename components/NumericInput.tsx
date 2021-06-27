c1c964e519f113f6481a60bd8df20221b0fd3dcf
Simon <simonvdhende@outlook.cok>
2021-06-27 20:48:46 +0200

    : int label

--- components/NumericInput.tsx
+++ components/NumericInput.tsx
@@ -3,7 +3,7 @@ import React, { ChangeEvent, FormEvent } from 'react';
 export const NumericInput = ({ label, value, setter }: { label: string; value: number; setter: any }) => {
   return (
     <div className="c-numeric-input">
-      <label className="c-numeric-input__label" htmlFor="label">{label}</label>
+      <label className="c-numeric-input__label" htmlFor="label">{label} : </label>
       <input className="c-input c-numeric-input__input" type="number" name={label} id={label} value={value} onChange={(e : FormEvent) => {
         setter(Math.max(1, +(e.target as HTMLTextAreaElement).value));
       }} />
