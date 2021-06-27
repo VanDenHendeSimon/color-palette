e97950fd51350d52bddf75fd97e6a2d986ae78ea
Simon <simonvdhende@outlook.cok>
2021-06-27 20:44:49 +0200

    numeric inputs and styles

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { FormEvent, useState } from 'react';
+import { NumericInput } from './NumericInput';
 
 export const Header = () => {
+
+  const [amountOfColors, setAmountOfColors] = useState<number>(3);
+  const [amountOfVariations, setAmountOfVariations] = useState<number>(11);
+
   return (
     <header className="c-header">
-      <h1>Palette</h1>
+      <div className="c-header__numeric-inputs">
+        <NumericInput value={amountOfColors} label="Colors" setter={setAmountOfColors} />
+        <NumericInput value={amountOfVariations} label="Variations" setter={setAmountOfVariations} />
+        <input className="c-input" type="button" value="test" onClick={() => {console.log(amountOfColors, amountOfVariations)}} />
+      </div>
+      <div></div>
+      <div></div>
     </header>
-  )
-}
+  );
+};
