7d8b656d8d11f41bb5076f033ff5421c80863ae6
Simon <simonvdhende@outlook.cok>
2021-06-29 14:54:29 +0200

    update description

--- data/DefaultSeoProps.tsx
+++ data/DefaultSeoProps.tsx
@@ -2,6 +2,6 @@ import { SEOProps } from "../models/SEOProps";
 
 export const defaultSeoProps: SEOProps = {
   title: "Color Palette",
-  description: "Easily choose and implement color schemes.",
+  description: "Generate color schemes with the most beautiful harmonies, combinations and relationships. Integrate into your app by exporting the palette to css and scss.",
   image: "https://tipperstorage.blob.core.windows.net/logos/og-image.png",
 };
